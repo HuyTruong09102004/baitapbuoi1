3f62c3410b9fb274a5b3e55d8c4347ee841d5fe0
HuyTruong09102004 <MarcoHuyTruong11@gmail.com>
2024-09-29 17:09:14 +0700

    init project

---
+++ Baitapdonhang/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baitapdonhang
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            try
+            {
+                //Nhập số lượng sản phẩm
+                Console.WriteLine("Nhap so luong san pham: ");
+                int soluong = int.Parse(Console.ReadLine());
+                //Nhập giá của một sản phẩm
+                Console.WriteLine("Nhap gia tri cua mot san pham: ");
+                double gia = Convert.ToDouble(Console.ReadLine());
+                //tính tổng giá trị đơn hàng
+                double tonggiatri = soluong * gia;
+                Console.WriteLine("Tong don hang la: " + tonggiatri);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Du lieu sai.Xin hay nhap lai. ");
+            }
+        }
+    }
+}
